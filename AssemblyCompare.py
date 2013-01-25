28eccf06d62f88d4a0cdfbd6d79e9a74d7857851
CharlesJB <Charles.Joly-Beauparlant@crchul.ulaval.ca>
2013-01-25 12:59:19 -0500

    AssemblyCompare.py: First part of comments in Sebastien Boisvert code review
    Signed-off-by: CharlesJB <Charles.Joly-Beauparlant@crchul.ulaval.ca>

--- AssemblyCompare.py
+++ AssemblyCompare.py
@@ -4,23 +4,22 @@
 # 2013-01-23
 
 """
-This script compares multiple assembly by k-merizing and coloring them, simulating
+This script compares multiple assemblies by k-merizing and coloring them, simulating
 what will eventually be done with greater efficiency with RayCompare plugin.
 
 Usage:
 ./AssemblyCompare.py <inputFile> <kmer-length>
 	inputFile: A file containing the path to every file to compare. One path per line.
 		   Files must be in fasta format.
-	kmer-length: Size of k-mer to produce.
+	kmer-length: Size of k-mers to produce.
 """
 
 class KmerParser:
 	def __init__(self, inputFile, kmerLength):
 		self.m_fileList = []
 		self._parseInputFile(inputFile)
-		self.m_kmerLength = int(kmerLength)
+		self.m_kmerLength = kmerLength
 		self.m_kmerList = {}
-		self.m_currentFilenameColorValue = 0
 
 	def parseFiles(self):
 		for filename in self.m_fileList:
@@ -44,30 +43,32 @@ class KmerParser:
 		print ""
 		print "VirtualColor\tCount\tPercentage"
 		for key in summary:
-			percentage = (float(summary[key]) / totalKmerCount) * 100.0
+			percentage = 0
+			if totalKmerCount != 0:
+				percentage = (float(summary[key]) / totalKmerCount) * 100.0
 			print self._printColor(key) + "\t" + str(summary[key]) + "\t" + str(percentage)
 		print ""
 		print "Unique k-mers total count\t" + str(totalKmerCount)
 
 	def _parseFile(self, filename):
-		self.m_currentFilenameColorValue = self._convertFilenameToColorValue(filename)
+		currentFilenameColorValue = self._convertFilenameToColorValue(filename)
 		entrySequence = ""
 		for line in open(filename):
 			if line[0] == '>':
-				self._kmerizeSequence(entrySequence)
+				self._kmerizeSequence(entrySequence.lower(), currentFilenameColorValue)
 				entrySequence = ""
 			else:
 				entrySequence = entrySequence + line.strip()
-		self._kmerizeSequence(entrySequence)
+		self._kmerizeSequence(entrySequence.lower(), currentFilenameColorValue)
 
-	def _kmerizeSequence(self, sequence):
-		if len(sequence) > self.m_kmerLength:
+	def _kmerizeSequence(self, sequence, currentFilenameColorValue):
+		if len(sequence) >= self.m_kmerLength:
 			start = 0
-			end = start + self.m_kmerLength
+			end = start + self.m_kmerLength - 1
 			while end < len(sequence):
-				self._addKmer(sequence[start:end], self.m_currentFilenameColorValue)
-				start = start + 1
-				end = end + 1
+				self._addKmer(sequence[start:end], currentFilenameColorValue)
+				start += 1
+				end += 1
 
 	def _addKmer(self, kmer, colorValue):
 		if kmer in self.m_kmerList:
@@ -110,7 +111,7 @@ if __name__=="__main__":
                 sys.exit(1)
 
 	inputFile = sys.argv[1]
-	kmerLength = sys.argv[2]
+	kmerLength = int(sys.argv[2])
 	kmerParser = KmerParser(inputFile, kmerLength)
 	kmerParser.parseFiles()
 	kmerParser.printSummary()
